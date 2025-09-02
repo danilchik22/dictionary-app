9e4a9f524b1201645240565a274a49ccd8d1e86b
Danil.Gushchin <dgushchin@themsteam.com>
2025-09-02 15:43:23 +0700

    fix dict

--- storage/init_database.go
+++ storage/init_database.go
@@ -2,11 +2,8 @@ package storage
 
 import (
 	"dictionary_app/internal/models"
-	"fmt"
-
-	"github.com/bxcodec/faker/v3"
-	"github.com/icrowley/fake"
-	"golang.org/x/exp/rand"
+	"encoding/json"
+	"os"
 )
 
 func InitNewDatabase() {
@@ -16,26 +13,64 @@ func InitNewDatabase() {
 	db.Raw(`
 	SELECT COUNT(ID) FROM dictionary
 	`).Scan(&count)
-	countWords := 0
 
-	if count < 100000 {
-		words := make(map[string]struct{})
-		for countWords < 100000 {
-			word := fmt.Sprintf("%s_%d", faker.Word(), rand.Intn(100000))
-			words[word] = struct{}{}
-			fmt.Println(len(words))
-			countWords = len(words)
-		}
-		i := 1
-		for item := range words {
-			def1 := fake.Word()
-			def2 := fake.Word()
-			dict1 := &models.Dictionary{Word: item, Definition: def1 + ", " + def2}
-			dictionaries = append(dictionaries, dict1)
-			i = i + 1
+	if count < 150000 {
+		words := readJsonToMap("filtered.json")
+		for word, meaning := range words {
+			if oneMeaning, ok := meaning["MEANINGS"]; ok {
+				if len(oneMeaning) != 0 {
+					newWord := ""
+					if len(oneMeaning) > 1 {
+						newWord = oneMeaning[0] + ", " + oneMeaning[1]
+					} else {
+						newWord = oneMeaning[0]
+					}
+
+					dict1 := &models.Dictionary{Word: word, Definition: newWord}
+					dictionaries = append(dictionaries, dict1)
+				}
+			}
+
 		}
 		batchSize := 1000
 		db.Table("dictionary").CreateInBatches(dictionaries, batchSize)
 	}
 
+}
+
+func readJsonToMap(filename string) map[string]map[string][]string {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	type entry struct {
+		MEANINGS [][]any  `json:"MEANINGS"`
+		ANTONYMS []string `json:"ANTONYMS"`
+		SYNONYMS []string `json:"SYNONYMS"`
+	}
+
+	var raw map[string]entry
+	if err := json.Unmarshal(data, &raw); err != nil {
+		panic(err)
+	}
+
+	result := make(map[string]map[string][]string, len(raw))
+	for word, e := range raw {
+		var defs []string
+		for _, m := range e.MEANINGS {
+			if len(m) > 1 {
+				if def, ok := m[1].(string); ok {
+					defs = append(defs, def)
+				}
+			}
+		}
+		result[word] = map[string][]string{
+			"MEANINGS": defs,
+			"ANTONYMS": e.ANTONYMS,
+			"SYNONYMS": e.SYNONYMS,
+		}
+	}
+
+	return result
 }
