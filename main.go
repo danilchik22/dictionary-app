4f69ba3502442b922c3b4e224164d1f2233cd33b
Danil.Gushchin <dgushchin@themsteam.com>
2025-09-08 15:28:00 +0700

    corses

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	mg "dictionary_app/migrations"
 	"dictionary_app/storage"
 	"log"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -21,15 +22,16 @@ func main() {
 	}
 	storage.InitNewDatabase()
 	server := gin.Default()
-	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Content-Type", "Authorization"},
-		AllowCredentials: true,
-	}))
 	server.SetTrustedProxies([]string{"192.168.3.60"})
 	server.Use(middleware.LoggerMiddleware())
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://81.177.48.223:5173", "http://localhost:5173", "http://192.168.3.60:8081"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD"},
+		AllowHeaders:     []string{"Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"},
+		AllowCredentials: true,
+		ExposeHeaders:    []string{"Content-Length", "Authorization"},
+		MaxAge:           12 * time.Hour,
+	}))
 	public := server.Group("/")
 	{
 		public.POST("/new_user", func(ctx *gin.Context) {
